2dc93bc222c31f621ca08d85addcedc195d0f5a0
LizzieDworsky <amydworsky15@gmail.com>
2024-02-28 07:21:18 -0700

    Updated API KEY variable name

--- netlify/functions/fetchAiApiKey.js
+++ netlify/functions/fetchAiApiKey.js
@@ -1,5 +1,5 @@
 exports.handler = async function (event, context) {
-    const API_KEY = process.env.aiApiKey; // Access the API key from environment variables
+    const API_KEY = process.env.AI_API_KEY; // Access the API key from environment variables
 
     return {
         statusCode: 200,
